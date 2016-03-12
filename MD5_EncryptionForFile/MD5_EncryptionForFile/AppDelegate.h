75c62cc15bcc378e6147970e4e9c4f8468f12732
薛尧 <xueyao@xueyaodeMacBook-Pro.local>
2016-03-12 19:46:38 +0800

    上传MD5加密文件Demo

---
+++ MD5_EncryptionForFile/MD5_EncryptionForFile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MD5_EncryptionForFile
+//
+//  Created by 薛尧 on 15/6/15.
+//  Copyright © 2016年 薛尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MD5_EncryptionForFile/MD5_EncryptionForFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MD5_EncryptionForFile
+//
+//  Created by 薛尧 on 15/6/15.
+//  Copyright © 2016年 薛尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
